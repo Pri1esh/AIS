d0189a81c70a0d9ee81a028971b4c33551802944
Homa Kaunain <ADVAIYASUP13@adani.com>
2024-10-08 19:21:53 +0530

    fixed popup on stage

--- src/components/HomePopUp/index.tsx
+++ src/components/HomePopUp/index.tsx
@@ -1,24 +1,19 @@
-import { CustomImage } from "@components";
-import styles from "./homePopUp.module.scss";
-import Modal from "react-bootstrap/Modal";
-import { useEffect, useState } from "react";
+import { CustomImage } from '@components';
+import styles from './homePopUp.module.scss';
+import Modal from 'react-bootstrap/Modal';
+import { useEffect, useState } from 'react';
 
 function MyVerticallyCenteredModal(props: any) {
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      className={styles.modalBody}
-    >
-      <Modal.Body className={styles.modalBox}>
+    <Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered className={styles.modalBox}>
+      <Modal.Body className={styles.modalBody}>
         <Modal.Header closeButton className={styles.modalHeader}></Modal.Header>
         <CustomImage
+          className={styles.modalImage}
           src={{
             mobileSource: props?.image[0]?.imageSourceMobile,
             tabletSource: props?.image[0]?.imageSourceTablet,
-            defaultSource: props?.image[0]?.imageSource
+            defaultSource: props?.image[0]?.imageSource,
           }}
           alt={props?.imageAlt}
         />
@@ -32,22 +27,15 @@ const HomePopUp = (props: any) => {
   const [modalShow, setModalShow] = useState(false);
 
   useEffect(() => {
-    if(!sessionStorage.getItem('PopUp-Show')){
+    if (!sessionStorage.getItem('PopUp-Show')) {
       setModalShow(true);
-      sessionStorage.setItem('PopUp-Show','true');
+      sessionStorage.setItem('PopUp-Show', 'true');
     }
-    
   }, []);
 
   return (
     <>
-      {compData && (
-        <MyVerticallyCenteredModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-          image={compData}
-        />
-      )}
+      {compData && <MyVerticallyCenteredModal show={modalShow} onHide={() => setModalShow(false)} image={compData} />}
     </>
   );
 };
